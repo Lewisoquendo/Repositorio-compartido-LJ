24c0f2f06ae0e89b48233cbe57845e6cb22ef4ec
Lewisoquendo <142113609+Lewisoquendo@users.noreply.github.com>
2023-08-16 18:46:47 -0400

    Añadiendo la carpeta de freecodecamp al gitignore

--- Archivos freecodecamp/Temas 1-50/12-crear-numeros-decimales.js
+++
@@ -1,9 +0,0 @@
-/*
-Curso de freeCodeCamp: "Aprende JavaScript - Curso Completo desde Cero".
-Curso Creado por: Estefania Cassingena Navone (@EstefaniaCassN).
-Tema: Crear números decimales con JavaScript.
-*/
-
-var miNumeroDecimal = 23.4;
-
-var peso = 78.34;
